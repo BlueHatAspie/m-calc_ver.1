54b02aac242831eb9d6397aca4c3b80f5f0aa96e
BlueHatCyberSpy <112766808+BlueHatCyberSpy@users.noreply.github.com>
2023-07-13 16:06:34 -0400

    removed debugging remnants

--- src/mcalc.h
+++ src/mcalc.h
@@ -3,10 +3,4 @@
 
 int evaluate(const char* equation);
 
-// === DEBUGGING ===
-enum Bool charInStr(const char argChar, const char* argStr);
-void EquationStepsAppend(struct EquationToken token, struct EquationToken tokens[]);
-enum Bool check(const char *equ);
-int read_equ(const char *equ, struct EquationToken tokens[]);
-
 #endif
